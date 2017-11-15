0b76de25f12d4021e2f5564432b735b4e60f2272
Mark He <1056977707@qq.com>
2017-11-15 15:21:53 -0500

    菜单

--- wp-content/plugins/fewen-article/fewen-article.php
+++ wp-content/plugins/fewen-article/fewen-article.php
@@ -14,11 +14,38 @@ Copyright 2017 Joysport inc.
 
 add_action( 'admin_menu', 'main_menu');
 
+// 加载 css js
+add_action( 'admin_enqueue_scripts', 'admin_enqueue_scripts' );
+
+
+
+function view( $file, $args=[] ){
+    foreach ($args as $k => $v){
+        $$k = $v;
+    }
+
+    include( $file . '.php' );
+}
+
+
 function main_menu(){
 	add_menu_page( 'article', 'article', 'manage_options', 'article', 'show');
 	remove_menu_page('article');
 }
 
 function show(){
-	echo 123;
+	view('article');
 }
+
+function admin_enqueue_scripts(){
+    wp_enqueue_style(
+        'fewen-article',
+        plugins_url( 'fewen-article/assets/css/main.css'  ),
+        array(),
+        '1.01',
+        'screen'
+    );
+}
+
+
+
